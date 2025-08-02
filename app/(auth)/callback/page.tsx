703b9b52741fc227af60d3d66b58600222f49da5
CodeFromRahul <rahulvish194002@gmail.com>
2025-08-03 01:31:02 +0530

    Added some changes in global css

---
+++ app/(auth)/callback/page.tsx
@@ -0,0 +1,9 @@
+// Todo :implement the auth callback logic here
+
+export const dynamic = "force-dynamic";
+
+const AuthCallbackPage = () => {
+    return <div>AuthCallbackPage</div>
+}
+
+export default AuthCallbackPage;
